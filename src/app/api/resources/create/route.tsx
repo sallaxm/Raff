c3fbc6a5635edc4aa24167b4f3b4509a3dda65ee
unknown <user.name>
2026-02-15 23:53:49 +0300

    fixed upload route with auto page counting

---
+++ src/app/api/resources/create/route.tsx
@@ -0,0 +1,14 @@
+[{
+	"resource": "/C:/Users/hp/qatar-uni-share/src/app/api/resources/create/route.tsx",
+	"owner": "typescript",
+	"code": "2322",
+	"severity": 8,
+	"message": "Type 'ArrayBuffer | SharedArrayBuffer' is not assignable to type 'ArrayBuffer'.\n  Type 'SharedArrayBuffer' is missing the following properties from type 'ArrayBuffer': resizable, resize, detached, transfer, transferToFixedLength",
+	"source": "ts",
+	"startLineNumber": 43,
+	"startColumn": 53,
+	"endLineNumber": 43,
+	"endColumn": 64,
+	"modelVersionId": 3,
+	"origin": "extHost1"
+}]
